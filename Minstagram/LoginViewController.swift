4b26f30370940f5e0b83cec15306c44db29abd77
Md Sadiq Sada <samin.sada@gmail.com>
2021-03-13 17:32:53 -0500

    add sign up with parse

--- Minstagram/LoginViewController.swift
+++ Minstagram/LoginViewController.swift
@@ -6,16 +6,37 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{ (success,error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                print("Error: \(String(describing: error?.localizedDescription))");
+            }
+        }
+          
+    }
+    
     /*
     // MARK: - Navigation
 
