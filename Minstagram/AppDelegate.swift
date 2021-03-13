443453d4c44026fded5665b605f1f1ab79bd7a9c
Md Sadiq Sada <samin.sada@gmail.com>
2021-03-13 16:56:04 -0500

    Add Parse Server

--- Minstagram/AppDelegate.swift
+++ Minstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "ZNVErPdXrNWjz7lGhCjzUjmE3VGl7Ti75BEzKuLe" // <- UPDATE
+                    $0.clientKey = "5eRr1SOWtxIEDiuvCUl3qpbEgnnk23kkUE9j37iv" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
+            
+        // --- end copy
+
+
         return true
     }
 
