1bb7dae2b6b90d72eaa3313613c13c34b17f47c3
Md Sadiq Sada <samin.sada@gmail.com>
2021-03-14 17:05:00 -0400

    add username and caption display to each post cell in tableview

---
+++ Minstagram/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Minstagram
+//
+//  Created by Md Sadiq Sada on 3/14/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
