0ed82dd17a7a47cff54207932414009961f0ca3e
Md Sadiq Sada <samin.sada@gmail.com>
2021-03-14 17:09:59 -0400

    add image view functionality to the post cells

--- Minstagram/FeedViewController.swift
+++ Minstagram/FeedViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -52,6 +53,11 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         cell.captionLabel.text = post["caption"] as? String
         
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
         
         return cell
         
